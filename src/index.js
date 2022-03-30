f601b47de13e2f392825451959ca78ddb9ff485d
Nomi <79703777+oimoengineer@users.noreply.github.com>
2022-03-30 17:35:54 +0900

    タスクの追加機能（テキスト）

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し初期化する
+  const inputTextId = document.getElementById("add-text");
+  const inputText = inputTextId.value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
